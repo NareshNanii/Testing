18842d52fd5cf05d1661cdd178508210f2a588b5
naresh <naresh@192.168.1.29>
2019-01-11 12:07:13 +0530

    pal

--- src/FB/Pali.java
+++ src/FB/Pali.java
@@ -3,7 +3,7 @@ package FB;
 class Pali{  
 	 public static void main(String args[]){  
 	  int r,sum=0,temp;    
-	  int n=454;//It is the number variable to be checked for palindrome  
+	  int n=121;//It is the number variable to be checked for palindrome  
 	  
 	  temp=n;    
 	  while(n>0){    
