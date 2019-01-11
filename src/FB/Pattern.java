885bc859303ea8d6a6ed7e7c433336c8b081475b
naresh <naresh@192.168.1.29>
2019-01-11 11:34:10 +0530

    alt

--- src/FB/Pattern.java
+++ src/FB/Pattern.java
@@ -11,13 +11,13 @@ public static void main(String[] args) {
 	System.out.println("pattern below");
 	for(int i=1; i<=rows;i++){
 		for(int j=1; j<=i; j++){
-			System.out.print(j);
+			System.out.print(j+" ");
 		}
 		System.out.println();
 	}
 	for(int i=rows-1;i>=1;i--){
 		for(int j=1; j<=i;j++){
-			System.out.print(j);
+			System.out.print(j+ " ");
 		}
 		System.out.println();
 	}
