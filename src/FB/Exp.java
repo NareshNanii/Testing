6d49b80c82d220073aa245c768ad22d7b2c261a7
naresh <naresh@192.168.1.11>
2019-01-31 10:41:30 +0530

    hy

--- src/FB/Exp.java
+++ src/FB/Exp.java
@@ -7,32 +7,32 @@ public class Exp {
 		
 		ArrayList<Integer> list = new ArrayList<Integer>();
 	
-	int a[] = {1,2,3,4,5};
-	int b[] = {5,6,7,8,9};
-	int c[] = {0,5,4,1,8,2,1};
+	int a[] = {1,2,3,4,5,8};
+	int b[] = {5,6,7,8,9,1};
+	//int c[] = {0,5,4,1,8,2,1};
 	
 	
 
-int x=0, y=0 , z=0;
+int x=0, y=0 ;
 
-while (x<a.length && y<b.length && z<c.length){
-	if (a[x]==b[y] && b[y]==c[z]){
+while (x<a.length && y<b.length){
+	if (a[x]==b[y]){
 		list.add(a[x]);
 		x++;
 		y++;
-		z++;
+		
 	}
 	else if(a[x]<b[y]) {
 		
 		x++;
 	}
-	else if (b[y]<c[z]){
+	else  {
 		y++;
 	}
-	else{
-		z++;
-	}
-	
+//	else{
+//		z++;
+//	}
+//	
 }
 System.out.println(list);
 }
--- src/Generic/BaseTest.java
+++ src/Generic/BaseTest.java
@@ -1,6 +1,5 @@
 package Generic;
 
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
@@ -15,7 +14,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
 
 @Listeners(Result.class)
 public abstract class BaseTest implements IAutoConst {
